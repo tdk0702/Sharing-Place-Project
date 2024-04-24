b397a402b5d79d3046c2d253ae0ba7893d959022
KoiH <22520702@gm.uit.edu.vn>
2024-04-24 15:27:03 +0700

    Create new project

---
+++ Sharing Place/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Sharing_Place
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Sharing Place/Login.xaml.cs
@@ -0,0 +1,9 @@
+namespace Sharing_Place;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+	}
+}
