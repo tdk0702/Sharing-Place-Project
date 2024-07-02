f014d547b75827175f85584338abc79703ae6270
Tran Manh Kien <22520711@gm.uit.edu.vn>
2024-07-02 23:09:39 +0700

    update

--- SPServer/obj/Debug/net8.0-windows/SPServer.AssemblyInfo.cs
+++ SPServer/obj/Debug/net8.0-windows/SPServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SPServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad47307f0ee8efae0a2b62b0a575078aa9410727")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cc77e918825e49c7b01284d7b671edec97fba0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SPServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SPServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Sharing Place/obj/Debug/net8.0-android/Sharing Place.AssemblyInfo.cs
+++ Sharing Place/obj/Debug/net8.0-android/Sharing Place.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sharing Place")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4a0f123ab272b695886d2bf77873e8b76772c531")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9cc77e918825e49c7b01284d7b671edec97fba0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sharing Place")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sharing Place")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
